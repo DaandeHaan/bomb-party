0aba0d88689458a69216bcd9ecc30d3260a7a272
Tami Elb <Tami.dev@outlook.com>
2025-01-17 16:47:51 +0100

    Working frontend with tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()], // Enable Vue plugin for Vite
-  server: {
-    port: 3000, // Development server port
-    open: true // Automatically open the browser on server start
-  },
-  build: {
-    outDir: 'dist', // Output directory for production build
-    emptyOutDir: true // Clean the output directory before building
-  },
-  resolve: {
-    alias: {
-      '@': '/src' // Shortcut to reference the `src` directory
-    }
-  }
-});
+  plugins: [vue()],
+})
