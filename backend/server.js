66c8bdbb263bafbeed381cb5e42f526c21287e26
Daan de Haan <daan@daan-helpt.nl>
2025-01-18 17:43:37 +0100

    cors disabled

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,13 @@ const wss = new WebSocket.Server({ server });
 
 socketService.init(wss);
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(require("./middleware/sessionMiddleware.js"));
 
 app.use('/api/game', require("./controllers/gameController.js"))
