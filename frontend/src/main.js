d5080d43bc3426880d03f533a2f2b11996f387eb
Tami Elb <Tami.dev@outlook.com>
2025-01-18 16:58:18 +0100

    Added toast and username input field

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import index from './router/index';
 import './style.css';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css"; // Import the Toast CSS
 
-createApp(App).use(index).mount('#app');
+const app = createApp(App);
+
+app.use(Toast);
+
+// Use router
+app.use(index);
+
+// Mount the app
+app.mount('#app');
