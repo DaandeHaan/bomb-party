5b094b893a98b0f7f44d4ab113dd0133954ca37d
Daan de Haan <daan@daan-helpt.nl>
2025-01-23 22:50:44 +0100

    added settings

--- frontend/src/components/lobby/Settings.vue
+++ frontend/src/components/lobby/Settings.vue
@@ -3,7 +3,7 @@
     <h1 class="text-text text-5xl font-bold pt-28 text-center">Settings</h1>
 
     <!-- Panel -->
-    <div class="bg-background w-[480px] h-[670px] border-2 border-border drop-shadow-xl p-8 flex flex-col gap-8">
+    <div class="bg-background w-[480px] border-2 border-border drop-shadow-xl p-8 flex flex-col gap-8">
 
       <!-- Field Difficulty -->
       <div>
@@ -19,6 +19,27 @@
         <select v-model="settings.language" id="language" class="w-full p-2 pl-0 text-text bg-transparent border-b border-primary-accent outline-none">
           <option v-for="option in languageOptions" :value="option" :key="option">{{ option }}</option>
         </select>
+      </div>
+
+      <!-- Field MaxPlayers -->
+      <div>
+        <label for="maxPlayers" min='2' class="text-text opacity-80 text-md font-normal">Max Players</label>
+        <input type="number" v-model="settings.maxPlayers" id="maxPlayers" class="w-full p-2 pl-0 text-text bg-transparent border-b border-primary-accent outline-none"></input>
+      </div>
+
+      <!-- Field Timer -->
+      <div>
+        <label for="timer" class="text-text opacity-80 text-md font-normal">Timer (In seconds)</label>
+        <input type="number" v-model="settings.defaultTimer" id="timer" class="w-full p-2 pl-0 text-text bg-transparent border-b border-primary-accent outline-none"></input>
+      </div>
+
+      <!-- Buttons Private Game -->
+      <div class="flex justify-between flex-col gap-2">
+        <label for="timer" class="text-text opacity-80 text-md font-normal">Publicity</label>
+        <div class="flex justify-between">
+          <button @click="settings.privateGame = true" :class="settings.privateGame ? 'bg-primary-accent' : 'bg-transparent' " class="transition-all duration-300 w-[48%] p-2 text-text text-md font-normal border-2 border-primary-accent">Private</button>
+          <button @click="settings.privateGame = false" :class="!settings.privateGame ? 'bg-primary-accent' : 'bg-transparent' " class="transition-all duration-300 w-[48%] p-2 text-text text-md font-normal border-2 border-primary-accent">Public</button>
+        </div>
       </div>
 
     </div>
