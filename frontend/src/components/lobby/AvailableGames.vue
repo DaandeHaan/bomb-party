5a4e5a9b607284bdea2411635314d69420cf2904
Daan de Haan <daan@daan-helpt.nl>
2025-01-24 07:47:52 +0100

    added available games

--- frontend/src/components/lobby/AvailableGames.vue
+++ frontend/src/components/lobby/AvailableGames.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="w-full">
+  <div class="w-full flex flex-col items-center gap-12">
+    <h1 class="text-text text-5xl font-bold pt-28 text-center">Available games</h1>
 
+    <!-- Games -->
+    <div class="w-full flex flex-col gap-8 px-24">
+      <GameCard v-for="game in games" :key="game.id" :game="game" />
+    </div>
   </div>
 </template>
 
 <script setup>
+import GameCard from './gameCard.vue';
+import { onMounted, ref } from 'vue';
+import { useToast } from 'vue-toastification';
+import axios from 'axios';
+
+const games = ref([]);
+const toast = useToast();
+
+onMounted(() => {
+  fetchGames();
+});
 
 const fetchGames = async () => {
   try {
